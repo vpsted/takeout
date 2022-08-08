85a6b1acbff04d6ba18f0bad1b9279f9ccff49ac
Cathy <2982552362@qq.com>
2022-08-08 22:28:12 +0800

    使用spring-data-redis

--- src/main/java/com/fang/takeout/config/WebMvcConfig.java
+++ src/main/java/com/fang/takeout/config/WebMvcConfig.java
@@ -1,19 +1,29 @@
 package com.fang.takeout.config;
 
 import com.fang.takeout.common.JacksonObjectMapper;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 //todome 2放行静态资源
 @Slf4j
 @Configuration//添加注解，使其能被识别为配置类
+@EnableSwagger2//Swagger
+@EnableKnife4j//Swagger
 public class WebMvcConfig extends WebMvcConfigurationSupport {
   /**
    * 设置静态资源映射，对静态资源进行放行
@@ -45,4 +55,32 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     //super.extendMessageConverters(converters);
   }
 
+  /**
+   * 用来创建一个Docket对象
+   * @return Docket对象
+   */
+  @Bean
+  public Docket createRestApi() {
+    // 文档类型
+    return new Docket(DocumentationType.SWAGGER_2)
+            //描述接口文档
+            .apiInfo(apiInfo())
+            .select()
+            //controller对应包结构
+            .apis(RequestHandlerSelectors.basePackage("com.fang.takeout.controller"))
+            .paths(PathSelectors.any())
+            .build();
+  }
+
+  /**
+   * 创建Docket对象的描述信息
+   * @return
+   */
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+            .title("瑞吉外卖")
+            .version("1.0")
+            .description("瑞吉外卖接口文档")
+            .build();
+  }
 }
